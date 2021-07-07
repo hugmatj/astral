bd36969d29f1aa602fe6e689f65f270846c3d775
Collin Henderson <collinpatrickhenderson@gmail.com>
2021-07-07 17:53:14 -0400

    Adds keyboard shortcut for search

--- resources/js/components/Galileo.vue
+++ resources/js/components/Galileo.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="p-4 border-b border-gray-300">
     <div class="relative flex items-center py-2 pr-3 transition-colors bg-white border-2 border-gray-300 rounded-md pl-9 focus-within:border-gray-400">
-      <input v-model="searchQuery" type="text" class="flex-grow w-full p-0 text-gray-500 placeholder-gray-300 border-none peer focus:ring-0" placeholder="Gaze through your telescope..." />
+      <input ref="input" v-model="searchQuery" type="text" class="flex-grow w-full p-0 text-gray-500 placeholder-gray-300 border-none peer focus:ring-0" placeholder="Gaze through your telescope..." aria-keyshortcuts="/" />
       <SearchIcon class="absolute w-5 h-5 text-gray-300 transition-colors pointer-events-none left-2 peer-focus:text-gray-400" aria-hidden="true" />
+      <span class="absolute px-2 py-1 text-xs font-bold text-gray-300 transition-opacity border border-gray-300 rounded-sm pointer-events-none right-2 peer-focus:opacity-0">/</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import { SearchIcon } from '@heroicons/vue/outline'
 import { useStarsFilterStore } from '@/store/useStarsFilterStore'
+import { onKeyStroke } from '@vueuse/core'
 
 export default defineComponent({
   components: { SearchIcon },
   setup () {
     const starsFilterStore = useStarsFilterStore()
 
+    const input = ref<HTMLInputElement>()
     const searchQuery = computed({
       get() {
         return starsFilterStore.searchQuery
@@ -26,8 +29,15 @@ export default defineComponent({
       }
     })
 
+    onKeyStroke('/', (e) => {
+      if (document.activeElement !== input.value) {
+        e.preventDefault()
+        input.value?.focus()
+      }
+    })
 
     return {
+      input,
       searchQuery,
     }
   }
