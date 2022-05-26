43827c92c43e04dd618c20ffad6fcb2a3e5cde8d
Collin Henderson <collinpatrickhenderson@gmail.com>
2022-05-26 12:23:58 -0400

    💩

--- resources/js/workers/githubStars.worker.ts
+++ resources/js/workers/githubStars.worker.ts
@@ -2,7 +2,6 @@ import { fetchStarsQuery } from '@/queries'
 import { FetchDirection } from '@/types'
 
 self.addEventListener('message', async ({ data }) => {
-  console.log('POOP')
   const cursor: string = data.cursor || null
   const direction: FetchDirection = data.direction || FetchDirection.DESC
 
